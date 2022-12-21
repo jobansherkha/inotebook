79d7cfd656845629892f73cc7a8b66f90ed96fee
jobansherkha <jobansherkha@gmail.com>
2022-12-20 21:07:21 -0500

    password hashing

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,32 +1,62 @@
-const express = require('express');
-const User = require('../modals/User');
-const router = express.Router()
+const express = require("express");
+const User = require("../modals/User");
+const router = express.Router();
+// importing bcrypt js, for adding password hashing, salt and pepper
+// npm install bcryptjs
+const bcrypt = require("bcryptjs");
 
-// importing thr express validator by following line 
+// importing thr express validator by following line
 
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-
-
-//  create a user using : Post "/api/auth" doesnot require auth
-router.post('/', [
-    body('email', 'please enter right email').isEmail(),
-    body('name', 'name should be at least 3 characters').isLength({ min: 3 }),
-    body('password', 'password should be 5 characters long').isLength({ min: 5 })
-], (req, res) => {
+//  create a user using : Post "/api/auth/createuser" doesnot require auth no login required
+router.post(
+  "/createuser",
+  [
+    body("email", "please enter right email").isEmail(),
+    body("name", "name should be at least 3 characters").isLength({ min: 3 }),
+    body("password", "password should be 5 characters long").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // if there are errors return bad requests
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    // using try catch to show error if any error occour
+    try {
+      // check whether the user with this email exists already
+      //   now we are using findone method to check that if the email is alredy exists in the database
+      //
 
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    }).then(user => res.json(user))
-     .catch(err =>{console.log(err)
-      res.json({error: 'please enter a unique value'})})
-   
-  })
+      let user1 = await User.findOne({ email: req.body.email });
+      if (user1) {
+        return res
+          .status(400)
+          .json({ error: "sorry this is already available" });
+      }
+      // making a slat for password hashing
+      const salt = await bcrypt.genSaltSync(10);
+      // setting password hashing
+      secPassword = await bcrypt.hash(req.body.password, salt);
+      let user = await User.create({
+        name: req.body.name,
+        password: secPassword,
+        email: req.body.email,
+      });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
 
-  module.exports = router
+    // .then(user => res.json(user))
+    //  .catch(err =>{console.log(err)
+    //   res.json({error: 'please enter a unique value'})})
+  }
+);
+
+module.exports = router;
